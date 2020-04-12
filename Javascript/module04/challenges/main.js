7421987a5c785de17ec637e2b6d3cc131dd444a6
r33-code <r33ngin33r@gmail.com>
2020-04-12 10:59:57 -0300

    New challenge almost done

--- Javascript/module04/challenges/main.js
+++ Javascript/module04/challenges/main.js
@@ -1,17 +1,22 @@
 function checkAge(age){
-    return new Promise(function(resolve, reject){
-        if(age >= 18){
-            resolve("Maior que 18");
-        }else{
-            reject("Menor que 18");
-        }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(age >= 18){
+                resolve();
+            }else{
+                reject();
+            }
+        },2000);
     });
 }
 
-checkAge(20)
-    .then(function(response){
-        console.log(response);
+checkAge(15)
+    .then(function(){
+        console.log("Maior que 18");
     })
-    .catch(function(error){
-        console.log(error);
+    .catch(function(){
+        console.log("Menor que 18");
+    })
+    .finally(function(){
+        console.log("Finalizado");
     });
