80e48fafd4ffa753a6681a9396c99fa87b3c6d2b
r33-code <r33ngin33r@gmail.com>
2020-05-08 18:01:24 -0300

    Working with NodeJS+Docker+MongoDB

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,9 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Initializing the app
 const app = express();
 
+//Initialing the database
+const dbURL = 'mongodb://192.168.99.101:27017/nodeapi';
+mongoose.connect(dbURL, 
+        { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+        },
+        () => {console.log("connection established")}).
+        catch(error => console.warn(error));
+
+mongoose.connection.on('connected', function(){  
+        console.log("Mongoose default connection is open to ", dbURL);
+ });
+mongoose.connection.on('error', function(err){
+    console.log("Mongoose default connection has occured "+err+" !");
+});
+mongoose.connection.on('disconnected', function(){
+    console.log("Mongoose default connection is disconnected");
+});
+
+requireDir('./src/models');
+
+
+
+const Product = mongoose.model('Product');
+
+//First route
 app.get('/', (req, res) => {
-    res.send('Hello, Hero');
+    Product.create({
+            title: 'React Native',
+            description: 'Build native apps with React Native',
+            url: 'http://github.com/facebook/react-ntive'
+    });
+    
+    return res.send('Hello, Hero!!!');
 });
 
 app.listen(3030);
