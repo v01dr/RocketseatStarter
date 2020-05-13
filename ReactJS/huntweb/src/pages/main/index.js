5ef8ead60ca97af9c60761601c88ff33126dd35e
r33-code <r33ngin33r@gmail.com>
2020-05-13 20:55:52 -0300

    More ReactJS code added

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
+import "./styles.css";
+
 export default class Main extends Component {
     state = {
         products: [],
@@ -17,11 +19,18 @@ export default class Main extends Component {
     }
 
     render() {
-        //return <h1>Contagem de produtos: {this.state.products.length}</h1>
+        const { products } = this.state;
+        
         return (
             <div className='product-list'>
-                {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
+                {products.map(product => (
+                    //<h2 key={product._id}>{product.title}</h2>
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         )
