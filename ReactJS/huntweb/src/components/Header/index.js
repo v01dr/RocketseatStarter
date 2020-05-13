2fefc3a595d50c3c57e73cb6c15aa04f220ae1d1
r33-code <r33ngin33r@gmail.com>
2020-05-12 23:19:39 -0300

    Playing with ReactJS

---
+++ ReactJS/huntweb/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./styles.css";
+
+/**
+class Header extends Component {
+    render(){
+        return <h1>Hello, Hero</h1>
+    }
+}
+ */
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
