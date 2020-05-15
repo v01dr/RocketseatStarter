374d5f167165fbb67315cad5a268b14fb95fae8c
r33-code <r33ngin33r@gmail.com>
2020-05-15 11:00:13 -0300

    End of the JSHunt, a ReactJS app

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import "./styles.css";
 
 export default class Main extends Component {
     state = {
         products: [],
-    }
+        productInfo: {},
+        page: 1,
+    };
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        this.setState({ products: response.data.docs });
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
+    };
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
     }
 
     render() {
@@ -29,9 +55,17 @@ export default class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="">Acessar</a>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={this.state.page === 1} onClick={this.prevPage}>
+                        Anterior
+                    </button>
+                    <button disabled={this.state.page === this.state.productInfo.pages} onClick={this.nextPage}>
+                        Próximo
+                    </button>
+                </div>
             </div>
         )
     }
